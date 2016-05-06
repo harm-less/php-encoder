2564aa51062f8704bf96f97353c6751ceb19bc13
Harm van der Werf <harmvdwerf@gmail.com>
2016-05-06 14:04:27 +0200

    Create two new classes for node child accessors

---
+++ src/PE/Nodes/Children/NodeChildAccessor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PE\Nodes\Children;
+
+class NodeChildAccessor {
+
+	private $method;
+
+	function __construct($method) {
+		$this->method = $method;
+	}
+
+}
+
+?>
---
+++ src/PE/Nodes/Children/NodeChildSetter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PE\Nodes\Children;
+
+class NodeChildSetter extends NodeChildAccessor {
+
+	function __construct($method) {
+		parent::__construct($method);
+	}
+
+}
+
+?>
